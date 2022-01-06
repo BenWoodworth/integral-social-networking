f61b6703e010812bcde1de9601f979293ff17487
BenWoodworth <ben@benwoodworth.net>
2022-01-06 17:37:49 -0500

    Implement SocialNetwork.publish() and .viewTimeline()

--- src/main/java/net/benwoodworth/katas/socialNetwork/SocialNetwork.java
+++ src/main/java/net/benwoodworth/katas/socialNetwork/SocialNetwork.java
@@ -1,13 +1,25 @@
 package net.benwoodworth.katas.socialNetwork;
 
-import java.util.List;
+import java.util.*;
 
 public final class SocialNetwork {
+    private final Map<User, Profile> profiles = new HashMap<>();
+
+    private Profile getProfile(User user) {
+        return profiles.computeIfAbsent(user, (key) -> new Profile());
+    }
+
     public void publish(User user, String message) {
-        // TODO
+        getProfile(user).posts.add(new Post(message));
     }
 
     public List<Post> viewTimeline(User alice) {
-        return null; // TODO
+        var timeline = new ArrayList<>(getProfile(alice).posts);
+        Collections.reverse(timeline);
+        return timeline;
+    }
+
+    private static class Profile {
+        final List<Post> posts = new ArrayList<>();
     }
 }
